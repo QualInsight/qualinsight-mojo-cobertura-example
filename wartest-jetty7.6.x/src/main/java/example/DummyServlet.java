7d19fcb5b5b2c53ae057833ffebb6fc8472acad6
michel <michel.pawlak@gmail.com>
2015-10-05 23:29:04 +0200

    #1 add examples

---
+++ wartest-jetty7.6.x/src/main/java/example/DummyServlet.java
@@ -0,0 +1,18 @@
+package example;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DummyServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws IOException {
+        response.getOutputStream()
+            .print("Hello");
+    }
+
+}
